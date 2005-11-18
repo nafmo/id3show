9018ff8d9d6b42af483e424010c197c771f9d194
Peter Karlsson <peter@softwolves.pp.se>
2005-11-18 08:00:32 +0000

    Fixed syntax errors

--- id3show.c
+++ id3show.c
@@ -42,11 +42,11 @@ char *genre[]= {"Blues", "Classic Rock", "Country", "Dance", "Disco",
                "Vocal", "Jazz+Funk", "Fusion", "Trance", "Classical",
                "Instrumental", "Acid", "House", "Game", "Sound Clip",
                "Gospel", "Noise", "AlternRock", "Bass", "Soul", "Punk",
-               "Space", "Meditative", "Instrumental Pop", "Instrumental
-               Rock", "Ethnic", "Gothic", "Darkwave", "Techno-Industrial",
-               "Electronic", "Pop-Folk", "Eurodance", "Dream", "Southern
-               Rock", "Comedy", "Cult", "Gangsta", "Top 40", "Christian
-               Rap", "Pop/Funk", "Jungle", "Native American", "Cabaret",
+               "Space", "Meditative", "Instrumental Pop", "Instrumental Rock",
+               "Ethnic", "Gothic", "Darkwave", "Techno-Industrial",
+               "Electronic", "Pop-Folk", "Eurodance", "Dream",
+               "Southern Rock", "Comedy", "Cult", "Gangsta", "Top 40",
+               "Christian Rap", "Pop/Funk", "Jungle", "Native American", "Cabaret",
                "New Wave", "Psychadelic", "Rave", "Showtunes", "Trailer",
                "Lo-Fi", "Tribal", "Acid Punk", "Acid Jazz", "Polka",
                "Retro", "Musical", "Rock & Roll", "Hard Rock", "Folk",
